f514271a58ec340f771c802fd83799740c689622
imrandevop <imranprvt11@gmail.com>
2025-09-12 12:33:38 +0530

    change name to VISIBLE

--- VISIBLE/settings.py
+++ VISIBLE/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "workflow.urls"
+ROOT_URLCONF = "VISIBLE.urls"
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "workflow.wsgi.application"
+WSGI_APPLICATION = "VISIBLE.wsgi.application"
 
 
 # Database
