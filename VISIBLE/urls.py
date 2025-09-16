df0cbd9a6a87d15973b85b0419ce3822721b31c3
imrandevop <imranprvt11@gmail.com>
2025-09-16 10:57:41 +0530

    changes for droplet setup

--- VISIBLE/urls.py
+++ VISIBLE/urls.py
@@ -1,22 +1,21 @@
-# workflow/urls.py
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({"message": "VISIBLE API", "version": "1.0", "status": "active"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/admin/', permanent=True)),  # Redirect root to admin
+    path('', RedirectView.as_view(url='/admin/', permanent=True)),
     
-    # Versioned API URLs
-    re_path(r'^api/(?P<version>v[0-9]+)/', include([
-        path('', include('apps.authentication.urls')),
-        path('profiles/', include('apps.profiles.urls')),
-        path('work-categories/', include('apps.work_categories.urls')),
-        # Add other app URLs here as you expand
-        # path('', include('apps.other_app.urls')),
-    ])),
+    # Simple test endpoint
+    path('api/1/', include('apps.authentication.urls')),
+    path('api/1/profiles/', include('apps.profiles.urls')),
+    path('api/1/work-categories/', include('apps.work_categories.urls')),
 ]
 
 # Serve media files in development
